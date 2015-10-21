5923a6684ec5237d341a3a74635922c878cb7cd8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-21 21:12:06 +0100

    fps added with fixed ball

--- js/game.js
+++ js/game.js
@@ -1,4 +1,16 @@
 (function () {
 	var canvas = document.getElementById('game-canvas'),
-		canvasContext = canvas.getContext('2d');
+		canvasContext = canvas.getContext('2d'),
+		fps = 30;
+
+	setInterval(updateAll, 1000 / fps);
+
+	function updateAll () {
+		canvasContext.fillStyle = 'black';
+		canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+		canvasContext.fillStyle = 'white';
+		canvasContext.beginPath();
+		canvasContext.arc(100, 100, 10, 0, Math.PI * 2, true);
+		canvasContext.fill();
+	}
 })();
