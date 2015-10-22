8b444f8061d16c1d326c264d7801225aca6241d5
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-22 08:29:33 +0100

    ball class added

--- js/game.js
+++ js/game.js
@@ -1,16 +1,41 @@
 (function () {
 	var canvas = document.getElementById('game-canvas'),
 		canvasContext = canvas.getContext('2d'),
-		fps = 30;
+		fps = 30,
+		ball;
 
 	setInterval(updateAll, 1000 / fps);
 
+	/* Ball Class */
+	var Ball = function (x, y, speedX, speedY) {
+		this.x = x;
+		this.y = y;
+		this.speedX = speedX;
+		this.speedY = speedY;
+	};
+
+	Ball.prototype.updatePosition = function () {
+		function _update(ball, coord, speed, boundary) {
+			ball[coord] += ball[speed];
+			if (ball[coord] > boundary || ball[coord] < 0) {
+				ball[speed] *= -1;
+			}
+		}
+
+		_update(this, 'x', 'speedX', canvas.width);
+		_update(this, 'y', 'speedY', canvas.height);
+	};
+	/* End Ball Class */
+
+
+	ball = new Ball(100, 100, 5, 7);
 	function updateAll () {
 		canvasContext.fillStyle = 'black';
 		canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 		canvasContext.fillStyle = 'white';
 		canvasContext.beginPath();
-		canvasContext.arc(100, 100, 10, 0, Math.PI * 2, true);
+		canvasContext.arc(ball.x, ball.y, 10, 0, Math.PI * 2, true);
 		canvasContext.fill();
+		ball.updatePosition();
 	}
 })();
