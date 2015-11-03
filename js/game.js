5576814243bceaa77129b78b3e8c7a7d95d06951
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-11-03 13:25:09 +0000

    calculation done on the edge of the ball

--- js/game.js
+++ js/game.js
@@ -36,10 +36,10 @@
 			this.reset();
 		}
 
-		// @TODO check the ball edges vs the paddle edge
-		if (this.y > paddle.y &&
-			this.y < paddle.y + PADDLE_THICKNESS &&
-			this.x > paddle.x && this.x < paddle.x + PADDLE_WIDTH
+		if (this.y + BALL_RADIUS > paddle.y &&
+			this.y - BALL_RADIUS < paddle.y + PADDLE_THICKNESS &&
+			this.x  + BALL_RADIUS > paddle.x &&
+			this.x - BALL_RADIUS < paddle.x + PADDLE_WIDTH
 		) {
 			this.speedY *= -1;
 
