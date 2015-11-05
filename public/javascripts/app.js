4159fc67beb59e1a220f9b60ed2afa10af6d0584
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-11-05 00:09:40 +0000

    canvas module used

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,21 +3,20 @@ if (typeof (require) != 'undefined') {
 }
 
 loader.executeModule('main',
-function () {
-	var canvas = document.getElementById('game-canvas'),
-		canvasContext = canvas.getContext('2d'),
-		fps = 30,
-		ball,
+'B', 'Canvas',
+function (B, canvas) {
+	var ball,
 		paddle,
 		bricks = [],
 		// position of the mouse in the canvas, taking in account the scroll
 		// and position of the canvas in the page
 		mouseX,
-		mouseY;
+		mouseY,
+		fps = 30;
+
+	canvas.init(document.getElementById('game-canvas'));
 
 	setInterval(updateAll, 1000 / fps);
-
-	canvas.addEventListener('mousemove', mouseMovedEvent);
 
 	/* Ball Class */
 	var Ball = function (x, y, speedX, speedY) {
@@ -32,14 +31,14 @@ function () {
 		this.y += this.speedY;
 
 		// This deals with the screen's edges
-		if (this.x - BALL_RADIUS < 0 || this.x + BALL_RADIUS > canvas.width) {
+		if (this.x - BALL_RADIUS < 0 || this.x + BALL_RADIUS > canvas.width()) {
 			this.speedX *= -1;
 		}
 		if (this.y - BALL_RADIUS < 0) {
 			this.speedY *= -1;
 		}
 		// The ball touches the bottom screen
-		if (this.y + BALL_RADIUS > canvas.height) {
+		if (this.y + BALL_RADIUS > canvas.height()) {
 			this.reset();
 			resetBricks(bricks);
 		}
@@ -80,7 +79,7 @@ function () {
 	};
 
 	Ball.prototype.draw = function () {
-		drawCircle(this.x, this.y, BALL_RADIUS, 'white');
+		canvas.drawCircle(this.x, this.y, BALL_RADIUS, 'white');
 	};
 	/* End Ball Class */
 
@@ -97,7 +96,7 @@ function () {
 	};
 
 	Paddle.prototype.draw = function () {
-		drawRectangle(this.x, this.y, this.w, this.h, 'white');
+		canvas.drawRectangle(this.x, this.y, this.w, this.h, 'white');
 	};
 	/* End Paddle Class */
 
@@ -112,7 +111,7 @@ function () {
 	}
 
 	Brick.prototype.draw = function () {
-		drawRectangle(this.x, this.y, this.w, this.h, 'red');
+		canvas.drawRectangle(this.x, this.y, this.w, this.h, 'red');
 	};
 
 	Brick.prototype.reset = function () {
@@ -125,12 +124,29 @@ function () {
 	};
 	/* End Brick Class */
 
-	ball = new Ball(canvas.width / 2, canvas.height / 2, 5, 7);
+	ball = new Ball(canvas.width() / 2, canvas.height() / 2, 5, 7);
 	paddle = new Paddle(
-		(canvas.width - PADDLE_WIDTH) / 2, canvas.height - 100,
+		(canvas.width() - PADDLE_WIDTH) / 2, canvas.height() - 100,
 		PADDLE_WIDTH,
 		PADDLE_THICKNESS
 	);
+
+	B.Events.on('mouse-moved', null, function (mX, mY) {
+		mouseX = mX;
+		mouseY = mY;
+		if (mouseX > PADDLE_WIDTH / 2 && mouseX < canvas.width() - PADDLE_WIDTH / 2) {
+			paddle.updatePosition(
+				Math.min(
+					Math.max(
+						0,
+						mouseX - PADDLE_WIDTH / 2
+					),
+					canvas.width() - PADDLE_WIDTH / 2
+				)
+			);
+		}
+	});
+
 
 	var col, row;
 	for (row = 0; row < BRICK_GRID_ROW; row++ ) {
@@ -153,55 +169,12 @@ function () {
 		}
 	}
 
-	/**
-	 * EVENT FOR THE MOUSE
-	 */
-	function mouseMovedEvent (e) {
-		var rect = canvas.getBoundingClientRect(),
-			root = document.documentElement;
-
-			mouseX = e.clientX - rect.left - root.scrollLeft;
-			mouseY = e.clientY- rect.top - root.scrollTop;
-
-		if (mouseX > PADDLE_WIDTH / 2 && mouseX < canvas.width - PADDLE_WIDTH / 2) {
-			paddle.updatePosition(
-				Math.min(
-					Math.max(
-						0,
-						mouseX - PADDLE_WIDTH / 2
-					),
-					canvas.width - PADDLE_WIDTH / 2
-				)
-			);
-		}
-	}
-
 	function moveAll () {
 		ball.updatePosition();
 	}
 
-	/**
-	 * Methods to draw stuff
-	 */
-	function drawRectangle (x, y, width, height, color) {
-		canvasContext.fillStyle = color;
-		canvasContext.fillRect(x, y, width, height);
-	}
-
-	function drawCircle (x, y, radius, color) {
-		canvasContext.fillStyle = color;
-		canvasContext.beginPath();
-		canvasContext.arc(x, y, radius, 0, Math.PI * 2, true);
-		canvasContext.fill();
-	}
-
-	function drawText (text, x, y, color) {
-		canvasContext.fillStyle = color;
-		canvasContext.fillText(text, x, y);
-	}
-
 	function drawAll () {
-		drawRectangle(0, 0, canvas.width, canvas.height, 'black');
+		canvas.drawRectangle(0, 0, canvas.width(), canvas.height(), 'black');
 		ball.draw();
 		paddle.draw();
 
@@ -212,7 +185,7 @@ function () {
 		}
 
 		if (DEBUG) {
-			drawText('(' +
+			canvas.drawText('(' +
 				Math.floor(mouseX / BRICK_SPACE_WIDTH) + ', ' +
 				Math.floor(mouseY / BRICK_SPACE_HEIGHT) + ', ' +
 				Brick.colRowToIndex(Math.floor(mouseX / BRICK_SPACE_WIDTH), Math.floor(mouseY / BRICK_SPACE_HEIGHT)) + ')', mouseX, mouseY, 'white');
