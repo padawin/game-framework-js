930226dc097bfcca47af73a463727e382affdec3
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-11-18 09:40:38 +0000

    do not allow more players than the map allows

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -34,7 +34,7 @@ function (B, canvas, Entities, Physics, Utils, data, Controls, Level, GUI) {
 
 			// Init the car
 
-			for (var p = 0; p < nbPlayers; p++) {
+			for (var p = 0; p < data.maps[0].start.length && p < nbPlayers; p++) {
 				var startCell = level.getCoordinatesCenterCell(data.maps[0].start[p][0], data.maps[0].start[p][1]),
 					car = new Entities.Car(startCell[0], startCell[1], Math.PI / 2, CAR_SPEED);
 				car.setGraphic(data.resources[data.resourcesMap.CAR].resource);
