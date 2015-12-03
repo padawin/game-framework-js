e3f51f9803a91b051061e18d7aeaa99309d8a827
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-12-03 18:46:56 +0000

    RACE - Unused variable removed

--- public/demos/race/javascripts/app.js
+++ public/demos/race/javascripts/app.js
@@ -17,7 +17,6 @@ function (B, Engine, canvas, Entities, GameEntities, Physics, Utils, data, Contr
 			{gasKey: Controls.KEY_UP_ARROW, reverseKey: Controls.KEY_DOWN_ARROW, leftKey: Controls.KEY_LEFT_ARROW, rightKey: Controls.KEY_RIGHT_ARROW},
 			{gasKey: Controls.KEY_W, reverseKey: Controls.KEY_S, leftKey: Controls.KEY_A, rightKey: Controls.KEY_D}
 		],
-		fps = 30,
 		urlParams = Utils.getUrlParams(window.location.search);
 
 	const DEBUG = urlParams.debug || NO_DEBUG;
