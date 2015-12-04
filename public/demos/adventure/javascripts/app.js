dce541de8b7a274d390f4abd118290548743987e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-12-04 10:40:35 +0000

    ADVENTURE - Adventure is a fixed size world

--- public/demos/adventure/javascripts/app.js
+++ public/demos/adventure/javascripts/app.js
@@ -22,7 +22,7 @@ function (B, Engine, Entities, GameEntities, Utils, data, Controls, Level) {
 
 	Engine.init(
 		document.getElementById('game-canvas'),
-		Engine.OPTION_USE_KEYBOARD | Engine.OPTION_USE_MOUSE,
+		Engine.OPTION_USE_KEYBOARD | Engine.OPTION_USE_MOUSE | Engine.OPTION_FIXED_SIZE_WORLD,
 		function () {
 			var startCell = Engine.getLevel().getCoordinatesCenterCell(data.maps[0].start[0], data.maps[0].start[1])
 			warrior = new GameEntities.Warrior('MyWarrior', startCell[0], startCell[1]);
